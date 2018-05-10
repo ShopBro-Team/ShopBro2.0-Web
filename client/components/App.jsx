8a3394632372ab7e741757911c75d71c42823bee
anugrah danang abiyoga <danangabiyoga26@gmail.com>
2018-05-10 15:38:52 +1200

    updated Nav component to now be Main

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,13 +3,13 @@ import {HashRouter as Router, Route} from 'react-router-dom'
 
 import Login from './Login'
 import Register from './Register'
-import Nav from './Nav'
+import Main from './Main'
 
 const App = () => (
   <Router>
     <div className='app-container has-text-centered'>
       <h1 className="title is-1">Shop Bro</h1>
-      <Route path="/" component={Nav} />
+      <Route path="/" component={Main} />
       <Route path="/login" component={Login} />
       <Route path="/Register" component={Register} />
     </div>
