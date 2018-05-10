6ea0a16c1d569af2cea057cc7eb5997a88147d8d
anugrah danang abiyoga <danangabiyoga26@gmail.com>
2018-05-10 16:00:34 +1200

    Login is now the first landing page to our app:

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,15 +5,25 @@ import Login from './Login'
 import Register from './Register'
 import Main from './Main'
 
-const App = () => (
+import {connect} from 'react-redux'
+
+const App = ({auth}) => (
   <Router>
     <div className='app-container has-text-centered'>
       <h1 className="title is-1">Shop Bro</h1>
-      <Route path="/" component={Main} />
+      <Route exact path='/' component={props => auth.isAuthenticated
+        ? <Main {...props} />
+        : <Login {...props} />
+      } />
+      {/* Above code (provided by Harrison) is the best practice way of writing React Routes */}
+      {/* The way we know and have written the turnery above before works but this is legit */}
+      <Route path="/main" component={Main} />
       <Route path="/login" component={Login} />
       <Route path="/Register" component={Register} />
     </div>
   </Router>
 )
 
-export default App
+const mapStateToProps = ({auth}) => ({auth})
+
+export default connect(mapStateToProps)(App)
