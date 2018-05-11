7953cf2c9b10ecf9f33b4c5d1f4b4b54f9816a14
anugrah danang abiyoga <danangabiyoga26@gmail.com>
2018-05-11 16:13:02 +1200

    clearing is done

--- client/components/ShoppingList.jsx
+++ client/components/ShoppingList.jsx
@@ -10,7 +10,9 @@ export class ShoppingList extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            id: 0
+            id: 0,
+            name: '',
+            cost: ''
         }
         this.handleChange = this.handleChange.bind(this)
         // this.handleChangeTwo = this.handleChangeTwo.bind(this)
@@ -31,6 +33,10 @@ export class ShoppingList extends React.Component {
                     cost_in_cents: this.state.cost}
         //console.log(item.id)      
         this.props.dispatch(addShoppingListItem(item))
+        this.setState({
+            name: '',
+            cost: ''
+        })
         //NOTE: Need to add functionality to reset add buttons to placeholder values - use reset?
       
     }
@@ -46,8 +52,8 @@ export class ShoppingList extends React.Component {
             </div> 
            <div className="field has-addons">
                  
-               <input onChange={this.handleChange} className="input is-medium" type="text" name="name" placeholder="Enter item" />
-               <input onChange={this.handleChange} className="input is-medium" type="text" name="cost" placeholder="Enter cost" />
+               <input onChange={this.handleChange} className="input is-medium" type="text" value={this.state.name} name="name" placeholder="Enter item" />
+               <input onChange={this.handleChange} className="input is-medium" type="text" value={this.state.cost} name="cost" placeholder="Enter cost" />
              
                <div className="control">
                 <a className="button is-medium is-primary is-outlined is-mobile" onClick={this.addItem} type="submit" value="add item">
