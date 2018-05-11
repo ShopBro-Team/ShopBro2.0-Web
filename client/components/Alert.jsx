b477b91620f6687999d66476cdc659879f58110f
anugrah danang abiyoga <danangabiyoga26@gmail.com>
2018-05-11 17:42:56 +1200

    alert visuals completed need to add functionality

--- client/components/Alert.jsx
+++ client/components/Alert.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+import Budget from './Budget'
+import ShoppingList from './ShoppingList'
+
 
 class Alert extends React.Component{
     render() {
         return <div>
-        <div className="notification is-danger">
+        <div className="notification is-warning">
             <p>You have gone over your budget!  Are you sure you want to
             continue?</p>
             <br/>
             <div className="buttons is-centered">
-                <a className="button is-centered">Yes</a>
-                <a className="button is-centered">No</a>
+                <a className="button is-centered"><Link to = '/budget'>Yes</Link></a>
+                <a className="button is-centered"><Link to = '/shoppinglist'>No</Link></a>
             </div>
         </div>
     </div>
--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -29,7 +29,6 @@ function Main (props) {
           </div>
         }
         <ShoppingList />
-        <Alert />
       </div>
       {/* <BudgetSetting /> */}
 
