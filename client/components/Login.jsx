dc0db4d71a006beaa3b803bb2c792f5dc6d5f656
student <--chache>
2018-05-12 15:15:38 +1200

    still trying to do the bulma

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -24,17 +24,34 @@ class Login extends React.Component {
   }
   render() {
     return (
-      <form className="Login container" onSubmit={this.submit}>
-        <label>Username:
-          <input className="input" type="text" name="user_name" onChange={this.updateDetails}/>
-        </label><br/>
-        <label>Password:
-          <input className="input" type="password" name="password" onChange={this.updateDetails}/>
-        </label><br/>
-        <input className="button is-success" type="submit" /><Link to= '/register'><a className="button is-light">Register</a></Link>
-      </form>
+      <div class="field is-grouped">
+        <form className="Login container" onSubmit={this.submit}>
+          <label>Username:
+             <input className="input" type="text" name="user_name" onChange={this.updateDetails}/>
+          </label><br/>
+          <label>Password:
+              <input className="input" type="password" name="password" onChange={this.updateDetails}/>
+          </label><br/>
+              <input className="button is-success" type="submit" /><Link to= '/register'><a className="button is-light">Register</a></Link>
+        </form>
+      </div> 
     )
   }
 }
 
 export default connect()(Login)
+
+
+
+{/* <div class="field is-horizontal">
+<div class="field-label is-normal">
+  <label class="label">Username</label>
+</div>
+<div class="field-body">
+  <div class="field">
+    <div class="control">
+      <input class="input" type="text" placeholder="username" />
+    </div>
+  </div>
+ </div> 
+</div> */}
