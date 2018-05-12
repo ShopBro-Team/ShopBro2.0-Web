ba498316283e653c0b1e72da964d682a73f9fba8
Rosie Burkett <burkettroz@gmail.com>
2018-05-12 19:19:48 +1200

    adds successfull tests for all budget and budgetView reducer types

---
+++ test/client/budget.reducer.test.js
@@ -0,0 +1,40 @@
+import budget from '../../client/reducers/budget'
+
+test('Budget Reducer initial state', () => {
+  const expected = ''
+  const action = {}
+
+  const actual = budget(undefined, action)
+
+  expect(actual).toEqual(expected)
+})
+
+test('ADD_BUDGET works', () => {
+    const expected = '500'
+      
+    const action = {
+        type: 'ADD_BUDGET',
+        budget: '500'
+    }
+  
+    const actual = budget(undefined, action)
+  
+    expect(actual).toEqual(expected)
+  
+  })
+
+
+  test('LOGOUT_SUCCESS works', () => {
+    const expected = ''
+      
+    const action = {
+        type: 'LOGOUT_SUCCESS',
+        isFetching: false,
+        isAuthenticated: false
+    }
+  
+    const actual = budget(undefined, action)
+  
+    expect(actual).toEqual(expected)
+  
+  })
