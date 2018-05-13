6e58f4b36a84ebab3453d339b2647ec928456368
Rosie Burkett <burkettroz@gmail.com>
2018-05-14 11:45:54 +1200

    adds two components to dashboard

--- client/components/dashboard/TotalSavings.jsx
+++ client/components/dashboard/TotalSavings.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 
 
 
-class TotalSavings extends React.Component {
-  render() {
+function TotalSavings (props) {
+
+  let userName = props.auth.user.user_name
+
+  function capitalizeFirstLetter(data) {
+    return data.charAt(0).toUpperCase() + data.slice(1);
+  }
+
+ 
     return (
       <div>
-        <h1>Hello World from TotalSavings</h1>
+        <h1>Hello {capitalizeFirstLetter(userName)} this is your Total Savings: $50</h1>
       </div>
     )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+    // totalSavings: state.totalSavings
+    //the above reducer has not been created yet steve suggested that he would bring in with api
   }
 }
 
-export default TotalSavings
+export default connect(mapStateToProps)(TotalSavings) 
