ebccd4e3314d24835d8daa963b8feda8532c319d
Annika <liebste.anniu@gmaile.com>
2018-05-15 11:40:29 +1200

    fixes date in shoppinglistinfo

--- client/components/dashboard/ShoppingListInfo.jsx
+++ client/components/dashboard/ShoppingListInfo.jsx
@@ -15,16 +15,22 @@ class ShoppingListInfo extends React.Component {
     this.state = {
       userName : props.auth.user.user_name,
     }
+    this.reverseString = this.reverseString.bind(this)
   }
 
+  reverseString(str) {
+    var array = []
+    array = str.split("-")
+    array.reverse()
+    str = array.join('.')
+    return str
+    }
 
 
   render() {
     return (
-
-
       <div>
-        <h1 className = "is-size-4">Hello {capitalizeFirstLetter(this.state.userName)}, this is your shoppinglist for {this.props.dashboardShoppingListById.length > 0 && this.props.dashboardShoppingListById[0].date.slice(0,10)}</h1>
+        <h1 className = "is-size-4">Hello {capitalizeFirstLetter(this.state.userName)}, this is your shoppinglist for {this.props.dashboardShoppingListById.length > 0 && this.reverseString(this.props.dashboardShoppingListById[0].date.slice(0,10))}</h1>
         <p className = "is-size-4">Total Budget: ${this.props.dashboardShoppingListById.length > 0 && this.props.dashboardShoppingListById[0].budget_in_cents/100}</p>
         <p className = "is-size-4">Total Savings: ${this.props.dashboardShoppingListById.length > 0 && this.props.dashboardShoppingListById[0].total_savings_in_cents/100}</p>
         {/* Above is a life cycle and it is async so what it means is that if props.dashboard... is true it will wait and render */}
