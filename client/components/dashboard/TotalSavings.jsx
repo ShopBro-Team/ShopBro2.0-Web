79f4c8e4ab59f8d8c0cf5abebf80d5a04c6cd1a4
anugrah danang abiyoga <danangabiyoga26@gmail.com>
2018-05-15 15:54:51 +1200

    Hello progress bar

--- client/components/dashboard/TotalSavings.jsx
+++ client/components/dashboard/TotalSavings.jsx
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {logoutUser} from '../../actions/logout'
 
+import CircularProgressbar from 'react-circular-progressbar'
+
 function TotalSavings (props) {
 
   //Conditional statement required as logout threw a props.auth.user was undefined  
@@ -17,6 +19,18 @@ function TotalSavings (props) {
         <div className="level-right">
           <button className="button" onClick={() => props.dispatch(logoutUser())}>Logout</button>
         </div>
+        <div className='columns is-mobile is-centered'>
+        <div className='column is-5'>
+        {console.log(props.totals[0])}
+        {props.totals[0] && <CircularProgressbar  percentage={(props.totals[0].totalsavings / props.totals[0].totalbudget)*100} 
+          textForPercentage={perc => `$${props.totals[0].totalsavings / 100}`} 
+          styles={{
+            path: { stroke: `rgba(244, 191, 68)`},
+            text: { fill: `rgba(49 ,55 ,68)` }
+          }}/>}
+        </div>
+        </div>
+        {/* <Progress type="circle" percent={100} status="success" /> */}
         <h1>Hello {capitalizeFirstLetter(userName)} this is your Total Savings : $  
          {props.totals[0] && (props.totals[0].totalsavings/100).toFixed(2)}</h1>
       </div>
