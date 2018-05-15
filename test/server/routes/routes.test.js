a79533430c272a42d62af830bed70e5456fb01d1
Rosie Burkett <burkettroz@gmail.com>
2018-05-16 11:24:26 +1200

    adds post routes testing

--- test/server/routes/routes.test.js
+++ test/server/routes/routes.test.js
@@ -1,7 +1,16 @@
 const request = require('supertest')
 // const cheerio = require('cheerio')
+var JWT = require('jsonwebtoken')
 
+var secret ='KRANG' 
+process.env.JWT_SECRET = secret
+// process.env.NODE_ENV = 'test'
 
+jest.mock('../../../server/db/shoppinglists', () => ({
+    addShoppinglist: () => Promise.resolve([
+        {id: 1, name:'harrison'}
+    ])
+}))
 
 // const server = require('../../client/components/App')
 const server = require('../../../server/server')
@@ -12,13 +21,21 @@ test('tests are working', () => {
     expect(true).toBe(true)
 })
 
-test('GET /login works', (done) => {
-    request(server)
-    .get('/api/auth')
-    .expect(302)
-    .end((err, res) => {
+test('POST /v1/shoppinglist works', () => {
+    var token = JWT.sign({ id:1,"name":"Harrison" }, secret);
+    const headers = {
+        Accept: 'application/json',
+        Authorization: "Bearer "+token
+  }
+  return request(server)
+    .post('/api/v1/shoppinglists')
+    .expect(201)
+    .set(headers)
+    .then(res => {
+        expect(res.body.length).toBeGreaterThan(0)
+    })
+    .catch((err, res) => {
         expect(err).toBeFalsy()
-        done()
     })
 })
 
