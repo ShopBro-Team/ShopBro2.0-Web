9b0bcb93d6e9320d4099a3128f0be78b3ff60dc3
Annika <liebste.anniu@gmaile.com>
2018-05-15 19:37:40 +1200

    adds actions and some react tests

---
+++ client/containers/BudgetSettingContainer.js
@@ -0,0 +1,6 @@
+import {connect} from 'react-redux'
+
+import BudgetSetting from '../components/BudgetSetting'
+
+
+export default connect()(BudgetSetting)
--- test/client/Components/BudgetSetting.test.js
+++ test/client/Components/BudgetSetting.test.js
@@ -1,24 +1,23 @@
-// import React from 'react'
-// import { shallow, mount } from 'enzyme'
+import React from 'react'
+import { shallow, mount } from 'enzyme'
 
-// import {BudgetSetting} from '../../../client/components/BudgetSetting'
-// // the above is just an example not sure how to actually do this for BudgetSetting when it's only exported with react/redux
+import BudgetSetting from '../../../client/components/BudgetSetting'
+import './setup-dom'
 
-// import './setup-dom'
 
-// import configureStore from 'redux-mock-store'
+// import configureStore from  'redux-mock-store'
 
 // const mockStore = configureStore([])
 
-// test('BudgetSetting with no budget', () => {
-//   const store = mockStore()
+// jest.mock('../../client/actions/greetings.js', () => ({
+//   getGreetings: () => ({
+//     type: 'FAKE_ACTION'
+//   })
+// }))
 
-//   const wrapper = shallow(<BudgetSetting
-//   greetings={[]} //this is copied directly from Harrison's example and will need to be changed here
-//   dispatch={store.dispatch}
-//   />)
+test('Text renders on BudgetSetting', () => {
+  const wrapper = shallow(<BudgetSetting />)
+  expect(wrapper.find('label').text()).toBe ('Set your budget $')
+})
 
-//   expect(wrapper.find('label').text()).toBe('Set your budget $')
-
-
-// })
+//need to check with Harrison/Kelly how to do further enzyme testing
