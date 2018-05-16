527d6f0e24fa4f6d4da4970288b3af6326eabf2f
anugrah danang abiyoga <danangabiyoga26@gmail.com>
2018-05-16 13:52:41 +1200

    tidied non auth server side db and routes

--- server/db/shoppinglists.js
+++ server/db/shoppinglists.js
@@ -1,12 +1,14 @@
-//This function inserts an object 'shoppinglist' into the 'shoppinglists' table.
-//Note : No capital L in the 'shoppinglist' or 'shoppingLists' variable name.
-//Note : The knex database is not called in this module. It is called in the route.
 
+//TO DO : No capital L in the 'shoppinglist' variable names.
+//NOTE : The knex database is not called in this module. It is called in the route.
+
+//Insert shopping list
 function addShoppinglist (shoppinglist, db) {
   return db('shoppinglists')
     .insert(shoppinglist)
 }
 
+//Get all shopping lists by user id
 function getShoppinglistsbyUserId (user_id, db) {
   return db('shoppinglists')
     .select() 
@@ -14,12 +16,14 @@ function getShoppinglistsbyUserId (user_id, db) {
     .orderBy('id', 'desc') 
 }    
 
+//Get a shopping list
 function getShoppinglistbyId(id, db) {
   return db('shoppinglists')
     .select()
     .where('id', id)
 } 
 
+//Delete a shopping list
 function deleteShoppinglistById(id, db) {
   return db('shoppinglists')
     .select()
@@ -27,6 +31,7 @@ function deleteShoppinglistById(id, db) {
     .delete()
 } 
 
+//Get shopping list totals by user id. Uses raw SQL statements.  
 function getTotalsByUserId (user_id, db) {
   return db('shoppinglists')
     .select('user_id', db.raw("SUM(total_savings_in_cents) as totalsavings"),
@@ -35,7 +40,7 @@ function getTotalsByUserId (user_id, db) {
     .groupByRaw('user_id')     
 }
 
-//Need to use ES2015 module exports as requiring in in routes file  
+//Need to use ES2015 module exports as 'requiring in' in the routes file  
 module.exports = {
   addShoppinglist,
   getShoppinglistsbyUserId,
