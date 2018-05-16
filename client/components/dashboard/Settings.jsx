9eabb46caacd55f905c2a15131acd19531398b13
Ruthbai <minjoobai@gmail.com>
2018-05-16 14:32:30 +1200

    setting half way

--- client/components/dashboard/Settings.jsx
+++ client/components/dashboard/Settings.jsx
@@ -10,7 +10,14 @@ function Settings (props) {
     <div className="level-right">
       <button className="button" onClick={() => props.dispatch(logoutUser())}>Logout</button>
     </div>
-  <h1>Hello Settings</h1>
+    <h1 className= "title is-4 has-text-warning has-text-weight-bold">Settings</h1>
+    <p className= "title is-5 has-text-warning has-text-weight-medium">Save your Shopping List</p>
+    <div className="buttons has-addons is-centered">
+      <span className="button is-success has-text-primary">Yes</span>
+      <span className="button is-warning is-selected has-text-primary">No</span>
+     </div>
+    <input className="input is-normal" type="text" name="name" placeholder="Change username" />
+    <button className="button">Submit</button>
   </div>
   )
 }
