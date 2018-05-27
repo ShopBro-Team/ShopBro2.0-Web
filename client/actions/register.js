e6fd2fa385ce1faf275f92200c360561a7887439
Annika <liebste.anniu@gmaile.com>
2018-05-27 14:16:42 +1200

    adds delete user button and function

--- client/actions/register.js
+++ client/actions/register.js
@@ -2,6 +2,45 @@ import request from 'superagent'
 import {saveUserToken} from '../utils/auth'
 import {receiveLogin} from './login'
 
+// Annika working on deleting user account, might not need this after all
+//deletes an existing item from redux state (through our shoppinglist reducer, hence item_id)
+// export function deleteShoppingListItem (item_id) {
+//   return {
+//     type: 'DELETE_ITEM',
+//     item_id
+//   }
+// }
+
+// as above, this is plan b trialling reset first
+// Dispatched in the deleteUserAccountById() below and deletes the specific user in store
+// export function deleteUserAccountByIdInStore (user_id) {
+//   return {
+//     type: 'DELETE_SHOPPINGLIST_BY_ID',
+//     user_id
+//   }
+// }
+
+export function resetApp() {
+  return {
+    type: 'RESET_APP'
+  }  
+}
+
+// Dispatched in the deleteUserAccount() function in Settings.jsx. Seletes the User from both store and database
+
+export function deleteUserAccountById (user_id) {
+  return (dispatch) => {
+    return request('delete', `auth/${user_id}`) //not sure about this
+      .then (res => {
+        dispatch(resetApp())
+      })
+      .catch(err => {
+        dispatch(showError(err.message))
+      })
+    }
+}
+
+
 export function registerUserRequest ({user_name, user_email, password}) {
   global.window.localStorage.setItem('fun', 'times')
   return (dispatch) => {
@@ -23,3 +62,5 @@ export function registerUserRequest ({user_name, user_email, password}) {
       })
   }
 }
+
+
