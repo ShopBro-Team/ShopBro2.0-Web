526ddf002703ee1a22c22f7fcca55b862a1a0d50
engie15 <e.matene15@gmail.com>
2018-06-08 13:57:24 +1200

    removed input in settings that had no functionality

--- client/components/dashboard/Settings.jsx
+++ client/components/dashboard/Settings.jsx
@@ -22,12 +22,6 @@ class Settings extends React.Component {
       <br/>
       <h1 className= "title is-4 has-text-warning has-text-weight-bold">Settings</h1>
 
-      <p className= "level-item is-5 has-text-warning has-text-weight-medium">Save your Shopping List</p>
-      <div className="buttons has-addons is-centered">
-        <span className="button is-success has-text-warning">Yes</span>
-        <span className="button is-selected has-text-warning">No</span>
-      </div>
-
       <p className= "level-item is-5 has-text-warning has-text-weight-medium">Delete your account</p>
       {/* ShopBro Team please leave line in here, will be used in future <input className="input is-normal has-text-centered" type="text" name="name" placeholder="Enter your password" /> */}
       <div className="buttons has-addons is-centered">
@@ -35,13 +29,6 @@ class Settings extends React.Component {
       </div>
       {/* ShopBro Team please leave line in here, will be used in future. Create an alert to confirm that user wants to delete account */}
 
-      <br/>
-        <p className= "level-item is-5 has-text-warning has-text-weight-medium">Change user name</p>
-      <input className="input is-normal has-text-centered" type="text" name="name" placeholder="Change username" />
-      <br/>
-      <br/>
-
-      <button className="button is-centered is-small is-warning has-text-primary">Submit</button>
     </div>
     )
   }
