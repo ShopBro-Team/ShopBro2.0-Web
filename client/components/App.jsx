9082a13d8b1130973217e5d8d0cffb5026c40da7
Rosie Burkett <burkettroz@gmail.com>
2018-06-15 21:01:12 +1200

    fix header margins

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -20,35 +20,37 @@ import { ShoppingList } from './ShoppingList';
 
 const App = ({auth}) => (
   <Router>
-    <div className='app-container has-text-centered is-block-flex-tablet is-block-flex-mobile ' >
-      <div className="header columns is-centered is-hidden-mobile">
-      <br/>
-        <img src="/image/144.png" alt="logo"  className="image"/>
-      </div>
-
-      <div className="header columns is-centered is-hidden-desktop is-hidden-tablet is-hidden-fullhd is-hidden-widescreen">
-      <br/>
-        <img src="/image/48.png" alt="logo"  className="image"/>
-      </div>
+    <div className='app-container has-text-centered is-block-flex-tablet is-block-flex-mobile is-block-flex-desktop' >
       
-      {/* <div className="page-content-mobile is-hidden-desktop is-hidden-tablet"> */}
-      <div className="page-content">
+      {/* logo sizing for larger than mobile */}
+      <div className='header columns is-centered is-hidden-mobile'>
+      <br/>
+        <img src='/image/144.png' alt='logo'  className='image'/>
+      </div>
+      {/* logo sizing for mobile */}
+      <div className='header columns is-centered is-hidden-desktop is-hidden-tablet is-hidden-fullhd is-hidden-widescreen'>
+      <br/>
+        <img src='/image/48.png' alt='logo'  className='image'/>
+      </div>      
+      
+      {/* <div className='page-content-mobile is-hidden-desktop is-hidden-tablet'> */}
+      <div className='page-content'>
         <Route exact path='/' component={props => auth.isAuthenticated
           ? <Main {...props} />
           : <Login {...props} />
         } />
         {/* Above code (provided by Harrison) is the best practice way of writing React Routes */}
         {/* The way we know and have written the turnery above before works but this is legit */}
-        <Route path="/main" component={Main} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/celebration" component={Celebration} />
-        <Route path="/budget" component={Budget} />
-        <Route path="/shoppinglist" component={ShoppingList} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/footer" component={Footer} />
-        <Route path="/settings" component={Settings} />
-        <Route path="/shoppinglistinfo" component={ShoppingListInfo} /> 
+        <Route path='/main' component={Main} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/celebration' component={Celebration} />
+        <Route path='/budget' component={Budget} />
+        <Route path='/shoppinglist' component={ShoppingList} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/footer' component={Footer} />
+        <Route path='/settings' component={Settings} />
+        <Route path='/shoppinglistinfo' component={ShoppingListInfo} /> 
       </div>
       {auth.isAuthenticated 
         && 
