1c354a9f8b8624401c4dc7ca45d831029c323ca0
Annika <liebste.anni@gmail.com>
2018-07-14 12:18:52 +1200

    adds reset to footer links when done shopping

--- client/components/dashboard/Footer.jsx
+++ client/components/dashboard/Footer.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-
-//Footer and navigation for app,  
+import { resetApp } from '../../actions/shoppinglist'
 
 function Footer (props) {
+
+  function reset(){
+    props.dispatch(resetApp())
+  }
  
   return (
     <div className="navbar is-active is-fixed-bottom ">
       <div className="buttons is-centered">
       <div className="field is-grouped is-centered">
-        <Link className="navbar-item is-dark" to="/dashboard">Dashboard</Link>
-        <Link className="navbar-item is-dark" to="/main">Main</Link>
-        <Link className="navbar-item is-dark" to="/settings">Settings</Link>
+        <Link className="navbar-item is-dark" to="/dashboard" onClick={reset}>Dashboard</Link>
+        <Link className="navbar-item is-dark" to="/main" onClick={reset}>Main</Link>
+        <Link className="navbar-item is-dark" to="/settings" onClick={reset}>Settings</Link>
         <Link className="navbar-item is-dark" to="/login" onClick={() => props.dispatch(logoutUser())}>Logout</Link>
         </div>
       </div>
@@ -19,6 +23,4 @@ function Footer (props) {
   )
 }
 
-
-
-export default Footer
+export default connect()(Footer)
