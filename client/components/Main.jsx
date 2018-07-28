b445e35f966e1877ecfef2898bffd8b8f2019ab6
engie15 <e.matene15@gmail.com>
2018-07-29 09:53:25 +1200

    refactored code and added alert component to Main component

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -36,6 +36,9 @@ class Main extends React.Component {
 
   render() {
     let userName = this.props.auth.user.user_name
+    const overBudget = this.props.budget - this.props.totalSpend < 0
+    const savings = this.props.budget - this.props.totalSpend > 0.01
+
     return (<div>
         <div className='Nav hero is-small is-success'>
           <div className='hero-body'>
@@ -51,13 +54,13 @@ class Main extends React.Component {
                 </div>
             }
             <ShoppingList />
-          {(this.props.budget - this.props.totalSpend) < 0 && <Alert noBudget={this.props.budget == 0} />} 
+          {overBudget && <Alert noBudget={this.props.budget == 0} />} 
           </div>
           
           {/* Done button calls done function and saves shopping list to database and celebrates if underbudget */}
           <div className='completed'>
             <button className='button is-normal is-warning has-text-white' onClick={() => this.done()}>
-              {this.props.budget - this.props.totalSpend > 0.01 ? <Link className='nav-item' to='/celebration'>Done</Link> : 
+              {savings ? <Link className='nav-item' to='/celebration'>Done</Link> : 
                 <Link className='nav-item' to='/dashboard'>Done</Link>}  
             </button>  
             <br/>
@@ -82,71 +85,3 @@ const mapStateToProps = (state) => {
 }
 
 export default connect(mapStateToProps)(Main)
-
-
-// function Main (props) {
-
-//   //Code lines 16 - 20 call in the user_name from the db and capitalise the first letter
-//   let userName = props.auth.user.user_name
-  
-//   function capitalizeFirstLetter(data) {
-//     return data.charAt(0).toUpperCase() + data.slice(1);
-//   }
-
-//   //function done is called as button onClick and dispatches save shoppingList with the following arguments: budget, total savings, date and shoppinglist array [ item id, name and cost]. If clause: if our user has a positive savings amount then the resetApp function gets called from the Celebration.jsx component. However, if our user does not have a positive savings amoungt they won't get redirected to this componet, hence why resetApp needs to get dispatched from the done button.
-//   function done() {
-//     props.dispatch(saveShoppingList(props.budget, 
-//       (props.budget - props.totalSpend), new Date(), props.shoppingList))
-
-//     if (props.budget - props.totalSpend < 0)  {
-//       props.dispatch(resetApp())
-//     } 
-
-//   }
-
-//   //props.auth.isAuthenticated checks if our user is authenticated, only if true can they progress to setting their budget, otherwise they see the Login and Register buttons which link to the relevant components. props.butdetView sets a nested ternary that navigates between BudgetSettingContainer and Budget. This is set as setting in initialState.
-//   return (
-//     <div>
-//     <div className='Nav hero is-small is-success'>
-//       <div className='hero-body'>
-//         <p className='is-4 has-text-warning has-text-weight-bold'>Kia ora {capitalizeFirstLetter(userName)}!</p> 
-//         <br/>
-//         {props.auth.isAuthenticated
-//           ? <div>
-//               {props.budgetView === 'setting' ? <BudgetSettingContainer /> : <Budget />}
-//             </div>
-//           : <div className='columns nav-menu'>
-//               <Link className='nav-item' to='/login'>Login</Link>&nbsp;
-//               <Link className='nav-item' to='/register'>Register</Link>
-//             </div>
-//         }
-//         <ShoppingList />
-//       {(props.budget - props.totalSpend) < 0 && <AlertContainer noBudget={props.budget == 0} />} 
-//       </div>
-      
-//       {/* Done button calls done function and saves shopping list to database and celebrates if underbudget */}
-//       <div className='completed'>
-//         <button className='button is-normal is-warning has-text-white' onClick={() => done()}>
-//           {props.budget - props.totalSpend > 0.01 ? <Link className='nav-item' to='/celebration'>Done</Link> : 
-//             <Link className='nav-item' to='/dashboard'>Done</Link>}  
-//         </button>  
-//         <br/>
-//         <br/>
-//       </div> 
-//     </div>
-//     </div>
-//   )
-// }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     auth: state.auth,
-//     budgetView: state.budgetView,
-//     budget: state.budget,
-//     totalSpend: state.totalSpend,
-//     shoppingList: state.shoppingList
-//   }
-// }
-
-// export default connect(mapStateToProps)(Main)
-
